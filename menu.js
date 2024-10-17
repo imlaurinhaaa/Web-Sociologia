f7103c4f88a45b01020b0dbbc0543b72412e30a7
LucasZanidaCostaLuz <158210099+LucasZanidaCostaLuz@users.noreply.github.com>
2024-10-17 11:23:03 -0300

    fix: menu height fixed

--- menu.js
+++ menu.js
@@ -11,19 +11,15 @@ function animarMenu(){
 function animar2(){
     const btn2 = document.getElementById('btn-menu2');
     btn2.classList.toggle('ativado');
+    btn2.addEventListener('hold', animarMenu2);
+
 }
 
 function animarMenu2(){
     const menu2 = document.getElementById('menu-menu');
     menu2.classList.toggle('aberto');
-    menu2.addEventListener('hold', animarMenu2);
 }
 
 function fecharMenu(){
-    const btn2 = document.getElementById('btn-menu2');
-    const btn = document.getElementById('btn-menu');
-
-    if(!btn.classList.toggle('ativo')){
-        return !btn2.classList.toggle('ativado')
-    }
+    
 }
