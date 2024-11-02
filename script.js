a32f067cd38af6eb654f82a427782af63908fef0
Evelyn <145203060+EvelynOliveira1200@users.noreply.github.com>
2024-11-02 15:18:15 -0300

    feat: create carrousel

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let currentIndex = 0;
+let currentIndexMovimentos = 0;
+let currentIndexSociologos = 0;
+let currentIndexAssuntos = 0;
 
-function slideLeft() {
-  const cardsContainer = document.querySelector(".cards");
-  const totalCards = document.querySelectorAll(".card").length;
+function slideLeft(containerSelector, currentIndex) {
+  const cardsContainer = document.querySelector(containerSelector);
+  const totalCards = cardsContainer.querySelectorAll(".card").length;
   const visibleCards = 3;
-  const cardWidth = document.querySelector(".card").offsetWidth + 20; // 20px de margem entre os cards
+  const cardWidth = cardsContainer.querySelector(".card").offsetWidth + 20; 
 
   if (currentIndex > 0) {
     currentIndex--;
@@ -13,13 +15,15 @@ function slideLeft() {
   }
 
   updateSlide(cardsContainer, currentIndex, cardWidth);
+
+  return currentIndex;
 }
 
-function slideRight() {
-  const cardsContainer = document.querySelector(".cards");
-  const totalCards = document.querySelectorAll(".card").length;
+function slideRight(containerSelector, currentIndex) {
+  const cardsContainer = document.querySelector(containerSelector);
+  const totalCards = cardsContainer.querySelectorAll(".card").length;
   const visibleCards = 3;
-  const cardWidth = document.querySelector(".card").offsetWidth + 20; // 20px de margem entre os cards
+  const cardWidth = cardsContainer.querySelector(".card").offsetWidth + 20; 
 
   if (currentIndex < totalCards - visibleCards) {
     currentIndex++;
@@ -28,8 +32,34 @@ function slideRight() {
   }
 
   updateSlide(cardsContainer, currentIndex, cardWidth);
+
+  return currentIndex;
 }
 
 function updateSlide(container, index, cardWidth) {
   container.style.transform = `translateX(-${index * cardWidth}px)`;
+}
+
+function slideLeftMovimentos() {
+  currentIndexMovimentos = slideLeft(".card-slider", currentIndexMovimentos);
+}
+
+function slideRightMovimentos() {
+  currentIndexMovimentos = slideRight(".card-slider", currentIndexMovimentos);
+}
+
+function slideLeftSociologos() {
+  currentIndexSociologos = slideLeft(".card-slider-2", currentIndexSociologos);
+}
+
+function slideRightSociologos() {
+  currentIndexSociologos = slideRight(".card-slider-2", currentIndexSociologos);
+}
+
+function slideLeftAssuntos() {
+  currentIndexAssuntos = slideLeft(".card-slider-3", currentIndexAssuntos);
+}
+
+function slideRightAssuntos() {
+  currentIndexAssuntos = slideRight(".card-slider-3", currentIndexAssuntos);
 }
